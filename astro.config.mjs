c81b3f956a7c5218432da5bbabd529e4b712dbea
DESKTOP-1P10BH1\lucas <lucas_sfc98@outlook.com>
2024-11-13 19:51:35 -0300

    Fix defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://Lucaspadularrosa/github.io',
+  site: 'https://Lucaspadularrosa.github.io',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
